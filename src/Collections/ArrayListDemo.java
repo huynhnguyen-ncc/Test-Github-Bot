860f64dd8fedad34c79cda12b204309a461bc656
QuineNguyen <huynh6255@gmail.com>
2026-04-24 12:33:40 +0700

    test commit

--- src/Collections/ArrayListDemo.java
+++ src/Collections/ArrayListDemo.java
@@ -9,6 +9,7 @@ public class ArrayListDemo {
 //      Collection là một interface trong Java đại diện cho một nhóm các đối tượng
 //      Cần phải chỉ rõ kiểu dữ liệu của Collection
 //      Nếu không chỉ rõ kiểu dữ liệu, Collection sẽ chứa các đối tượng kiểu Object
+//      Collection
         List<Integer> nums = new ArrayList<Integer>();
         nums.add(6);
         nums.add(5);
